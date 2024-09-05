f2a9f188ab7f98141cd9b81f3826620f8dfdaf4b
PyroGiirl <pyrogiirl@outlook.com>
2024-09-05 11:57:51 -0400

    add func

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/PyroGiirl/libarraytest
+
+go 1.23.0
---
+++ lib.go
@@ -0,0 +1,21 @@
+package libarraytest
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+// ArrayGenerate retourne un tuple de nombres aléatoires de la taille spécifiée.
+func ArrayGenerate(size int) ([]int, error) {
+	if size <= 0 {
+		return nil, fmt.Errorf("la taille doit être un nombre entier positif")
+	}
+
+	rand.Seed(time.Now().UnixNano())
+	result := make([]int, size)
+	for i := 0; i < size; i++ {
+		result[i] = rand.Intn(10000) + 1
+	}
+	return result, nil
+}
